6320b23e6082eaed721279afa4467a8984594f7d
game428 <719297411@qq.com>
2018-04-28 14:12:54 +0800

    4.28 14:12

--- js/phoneTest.js
+++ js/phoneTest.js
@@ -1,3 +1,41 @@
-/**
- * Created by adminuser on 2018/4/28.
- */
+getCallBack({},"/dabai-chaorenjob/seeker/getUserSeekerInfo",initMy)
+var verificationId;
+function initMy(res){
+  if(res.code == 1){
+    $(".phone_show span").text(res.data.mobile)
+  }
+  console.log(res)
+}
+$(".code_btn").click(function(){
+  var phone = $('.phone_show span').text();
+  if(!phone){
+    return;
+  }
+  postCallBack({},"/dabai-chaorenjob/seeker/changeMobileBeforeVerificationId",getCode)
+})
+function getCode (res){
+  if(res.code == 1){
+    verificationId = res.data;
+  }
+  console.log(res)
+}
+$('.js_next').click(function(){
+  var code = $(".js_code").val();
+  if(!/^[0-9]{4}$/.test(code)){
+    return;
+  }else if(!verificationId){
+    alert("请重新获取验证码")
+    return;
+  }
+  var postData = {
+    verificationId:verificationId,
+    verificationCode:code
+  }
+  postCallBack(postData,"/dabai-chaorenjob/seeker/changeMobileSureBeforeVerification",nextStep)
+})
+function nextStep(res){
+  if(res.code == 1){
+    window.location.href = "phoneNew.html"
+  }
+  console.log(res)
+}
