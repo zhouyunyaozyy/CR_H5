d16a696f6145b6d240ccc65a69e704ef97d58143
game428 <719297411@qq.com>
2018-05-02 14:26:50 +0800

    5.2 14:30

--- js/completeInfo.js
+++ js/completeInfo.js
@@ -1,8 +1,12 @@
 var comPhone,comCode,appid;
 $(function(){
   var url_obj =url_analysis(window.location.search)
+  getCallBack({code:url_obj.code},"/dabai-chaorenjob/seeker/getWeChatOpenId",getOpenId)
   comPhone = url_obj.phone;
   comCode = url_obj.code;
   appid = url_obj.appid;
   console.log(url_obj)
 })
+function getOpenId(res){
+  console.log(res)
+}
