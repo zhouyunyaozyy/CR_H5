a29502a8dcda261bf3ffd84da222f319ec7570b9
game428 <719297411@qq.com>
2018-05-03 18:00:12 +0800

    5.3 18:01

--- js/common.js
+++ js/common.js
@@ -76,10 +76,10 @@ function token (){
   return resultData;
 }
 //接口公共url
-var locationIp =  'http://192.168.1.115:5020';
-//var locationIp =  'http://api.chaorenjob.com';
-//var resourceUrl = "http://h5.chaorenjob.com"
-var resourceUrl = "http://192.168.1.115:5020/dabai-page"
+//var locationIp =  'http://192.168.1.115:5020';
+var locationIp =  'http://api.chaorenjob.com';
+var resourceUrl = "http://h5.chaorenjob.com"
+//var resourceUrl = "http://192.168.1.115:5020/dabai-page"
 //ajax
 function getCallBack(data,url,success,isData){
   var resultData = token();
